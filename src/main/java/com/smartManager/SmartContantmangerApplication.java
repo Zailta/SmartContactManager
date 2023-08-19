9ac15833f55e9ac22ccd87e19cfe05414e01cd94
mohd.mohammad.yaqoob <mohd.mohammad.yaqoob@BDC6-LX-13423JM.dir.svc.accenture.com>
2023-08-19 15:00:29 +0530

    project Creation - first commit

---
+++ src/main/java/com/smartManager/SmartContantmangerApplication.java
@@ -0,0 +1,13 @@
+package com.smartManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContantmangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContantmangerApplication.class, args);
+	}
+
+}

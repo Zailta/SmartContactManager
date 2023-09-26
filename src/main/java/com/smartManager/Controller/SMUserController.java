800f3edb003199a81fb01706734b6c7096e51f03
Zailta <mannanbhat60@gmail.com>
2023-09-26 16:05:17 +0530

    Spring Security Initialization

---
+++ src/main/java/com/smartManager/Controller/SMUserController.java
@@ -0,0 +1,23 @@
+package com.smartManager.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "user")
+public class SMUserController {
+
+	public SMUserController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@GetMapping(value = "/dashboard")
+	public ModelAndView openHomePage(ModelAndView modelAndView) {
+		modelAndView.addObject("User-DashBoard", "Home - Smart Contact Manager");
+		modelAndView.setViewName("SMUserDashboard");
+		return modelAndView;
+	}
+
+}

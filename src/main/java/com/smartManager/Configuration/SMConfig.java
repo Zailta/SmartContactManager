4d703fad6ccc6f71ebc63246716820efb152f3ea
Zailta <mannanbhat60@gmail.com>
2023-09-27 17:56:45 +0530

    Spring Security Configuration

--- src/main/java/com/smartManager/Configuration/SMConfig.java
+++ src/main/java/com/smartManager/Configuration/SMConfig.java
@@ -1,8 +1,55 @@
 package com.smartManager.Configuration;
 
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 @Configuration
-public class SMConfig {
-
+@EnableWebSecurity
+public class SMConfig extends WebSecurityConfiguration{
+	@Bean
+	public UserDetailsService getUserDetailsService() {
+		return new SMUserDetailsService();
+	}
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider getDaoAuthenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(this.getUserDetailsService());
+		authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+		return authenticationProvider;
+	}
+	 @Bean
+	  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		 httpSecurity.authorizeHttpRequests((authorize) -> {
+			try {
+				authorize
+						 	.requestMatchers("/admin/**").hasRole("ADMIN")
+							.requestMatchers("/user/**").hasRole("USER")
+							.requestMatchers("/**").permitAll();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		 
+		 return httpSecurity.build();
+	 }
+	
+	
 }

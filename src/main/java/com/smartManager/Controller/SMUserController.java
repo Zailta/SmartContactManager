f16a7ffbb83efd599b5f4e514311906bab5d57d1
Zailta <mannanbhat60@gmail.com>
2023-10-03 23:03:24 +0530

    Working on User Dashboard

--- src/main/java/com/smartManager/Controller/SMUserController.java
+++ src/main/java/com/smartManager/Controller/SMUserController.java
@@ -1,20 +1,31 @@
 package com.smartManager.Controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.smartManager.DAO.SMUserRepository;
+import com.smartManager.Entity.SMUserEntity;
+
 @Controller
 @RequestMapping(value = "/user")
 public class SMUserController {
 
+	@Autowired
+	SMUserRepository repository;
 	public SMUserController() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	@GetMapping(value = "/dashboard")
-	public ModelAndView openHomePage(ModelAndView modelAndView) {
+	public ModelAndView openHomePage(ModelAndView modelAndView, Principal principal) {
+		String userName = principal.getName();
+		SMUserEntity findByEmail = repository.findByEmail(userName);
+		modelAndView.addObject("user", findByEmail);
 		modelAndView.addObject("User-DashBoard", "Home - Smart Contact Manager");
 		modelAndView.setViewName("Generic/SMUserDashboard");
 		return modelAndView;

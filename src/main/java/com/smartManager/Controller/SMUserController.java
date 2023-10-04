d140c6e63de6b7cc1b1164ae610a1f7288d089a2
Zailta <mannanbhat60@gmail.com>
2023-10-04 23:19:25 +0530

    change to add new contact for a given user

--- src/main/java/com/smartManager/Controller/SMUserController.java
+++ src/main/java/com/smartManager/Controller/SMUserController.java
@@ -5,10 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.smartManager.DAO.SMUserRepository;
+import com.smartManager.Entity.SMContactEntity;
 import com.smartManager.Entity.SMUserEntity;
 
 @Controller
@@ -21,13 +23,25 @@ public class SMUserController {
 		// TODO Auto-generated constructor stub
 	}
 	
-	@GetMapping(value = "/dashboard")
-	public ModelAndView openHomePage(ModelAndView modelAndView, Principal principal) {
+	@ModelAttribute
+	public void addCommonDataAttribute(ModelAndView modelAndView, Principal principal) {
 		String userName = principal.getName();
 		SMUserEntity findByEmail = repository.findByEmail(userName);
 		modelAndView.addObject("user", findByEmail);
+	}
+	
+	@GetMapping(value = "/dashboard")
+	public ModelAndView openHomePage(ModelAndView modelAndView, Principal principal) {
 		modelAndView.addObject("User-DashBoard", "Home - Smart Contact Manager");
 		modelAndView.setViewName("Generic/SMUserDashboard");
+		return modelAndView;
+	}
+	
+	@GetMapping(value = "/add-contact")
+	public ModelAndView openAddContact(ModelAndView modelAndView, Principal principal) {
+		modelAndView.addObject("newContact", new SMContactEntity());
+		modelAndView.addObject("Add-Contact", "Smart Contact Manager");
+		modelAndView.setViewName("Generic/SMAddNewUser");
 		return modelAndView;
 	}
 

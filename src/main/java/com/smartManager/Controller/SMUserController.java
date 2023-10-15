c2b4937e3ed4cd1b2f9b459ceb518714572b63ee
Zailta <mannanbhat60@gmail.com>
2023-10-15 17:24:20 +0530

    Enabling Email for Contact manager

--- src/main/java/com/smartManager/Controller/SMUserController.java
+++ src/main/java/com/smartManager/Controller/SMUserController.java
@@ -10,10 +10,13 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +24,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -42,6 +47,8 @@ public class SMUserController {
 	SMUserRepository repository;
 	@Autowired
 	SMContactRepository contactRepository;
+	@Autowired
+	JavaMailSender javaMailSender;
 
 	public SMUserController() {
 		// TODO Auto-generated constructor stub
@@ -155,8 +162,57 @@ public class SMUserController {
 		return modelAndView;
 	}
 	@PostMapping(value = "/invoke-email")
-	public ModelAndView processEmail(ModelAndView modelAndView, @RequestParam("contactID") Integer contactID, Principal principal, HttpSession session) {
-		
+	public ModelAndView processEmail(ModelAndView modelAndView, Principal principal, HttpSession session) {
+		Integer contactID = Integer.valueOf(((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getParameter("contactID"));
+		Optional<SMContactEntity> findById = contactRepository.findById(contactID);
+		if(principal.getName().equals(findById.get().getEmail())) {
+			String to = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getParameter("to");
+			String from = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getParameter("from");
+			//code to invoke Email Service:
+			StringBuilder body = new StringBuilder();
+			body.append("<table class=\"table\">\r\n"
+					+ "        <thead>\r\n"
+					+ "          <tr>\r\n"
+					+ "			  <th>Name</th>\r\n"
+					+ "              <th>Email</th>\r\n"
+					+ "              <th>About</th>\r\n"
+					+ "              <th>Phone Number</th>\r\n"
+					+ "              <th>Nick Name</th>\r\n"
+					+ "          </tr>\r\n"
+					+ "        </thead>\r\n"
+					+ "\r\n"
+					+ "        <tbody>\r\n"
+					+ "       ");
+			body.append("<tbody>\r\n"
+					+ "			<tr>\r\n"
+					+ "				<td>\r\n"
+					+ "					<a>\r\n"
+					+ "						<div>\r\n"
+					+ "							<span "+findById.get().getName()+"\"></span>\r\n"
+					+ "						</div>\r\n"
+					+ "					</a>\r\n"
+					+ "				</td>\r\n"
+					+ "				<td "+findById.get().getEmail()+"\"></td>\r\n"
+					+ "				<td "+findById.get().getAbout()+"\"></td>\r\n"
+					+ "				<td "+findById.get().getPhone()+"\"></td>\r\n"
+					+ "				<td "+findById.get().getNickName()+"\"></td>\r\n"
+					+ "			</tr>\r\n"
+					+ "		</tbody>\r\n"
+					+ "		</table>");
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+			mailMessage.setSubject("Smart Contact Manager: Contact-Details");
+			mailMessage.setTo(to); 
+			mailMessage.setFrom(from);
+			mailMessage.setText(body.toString()); 
+			javaMailSender.send(mailMessage);
+			
+			modelAndView.addObject("contact",findById.get());
+			modelAndView.addObject("useremail",principal.getName());
+			modelAndView.setViewName("Generic/SMEmailForm");
+		}
+		else {
+			session.setAttribute("message", new SMMessageHandler("You don't have Email priveleges for this contact! ", "alert-danger"));
+		}
 		return modelAndView;
 	}
 	
